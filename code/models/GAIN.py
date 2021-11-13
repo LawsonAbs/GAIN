a28853a5cfe4753250211612e7248c3dd04afdca
LawsonAbs <shenliu@ahnu.edu.cn>
2021-11-13 16:12:40 +0800

    理解图操作

--- code/models/GAIN.py
+++ code/models/GAIN.py
@@ -270,20 +270,23 @@ class GAIN_BERT(nn.Module):
 
         for i in self.GCN_layers:
             i.cuda() # 移入到GPU中
-        # TODO bank_size 为啥是这么计算的？ self.config.gcn_layers 表示的是gcn 的层数， +1 表示的是初始化的embedding维度。所以这里是 *(self.config.gcn_layers+1)。 这个可以在论文中的3.2下的公式(4)得出
+        # bank_size 为啥是这么计算的？ self.config.gcn_layers 表示的是 gcn 的层数， +1 表示的是初始化的embedding维度。所以这里是 *(self.config.gcn_layers+1)。 这个可以在论文中的3.2下的公式(4)得出
         self.bank_size = self.gcn_dim * (self.config.gcn_layers + 1)
         self.dropout = nn.Dropout(self.config.dropout)
         
-        # 相当于只在图神经网络之后，紧跟了这么几层用于最后的预测操作
+        # 图神经网络之后，紧跟了这么几层用于最后的预测操作
         self.predict = nn.Sequential(
             nn.Linear(self.bank_size * 5 + self.gcn_dim * 4, self.bank_size * 2),
             self.activation,
             self.dropout,
             nn.Linear(self.bank_size * 2, config.relation_nums),
         )
-        # 查看这个类的定义，是想做什么？
-        self.edge_layer = RelEdgeLayer(node_feat=self.gcn_dim, edge_feat=self.gcn_dim,
-                                       activation=self.activation, dropout=config.dropout)
+        # TODO 查看这个类的定义，是想做什么？ => 跟上面的这个 RelGraphConvLayer 有什么区别？         
+        self.edge_layer = RelEdgeLayer(node_feat=self.gcn_dim, 
+                                       edge_feat=self.gcn_dim,
+                                       activation=self.activation,
+                                       dropout=config.dropout
+                                       )
 
         self.path_info_mapping = nn.Linear(self.gcn_dim * 4, self.gcn_dim * 4)
 
@@ -341,7 +344,7 @@ class GAIN_BERT(nn.Module):
                 (torch.arange(mention_num) + 1).unsqueeze(1).expand(-1, slen))  # [mention_num, slen]
             mentions = mention_id[i].unsqueeze(0).expand(mention_num, -1)  # [mention_num, slen]
             select_metrix = (mention_index == mentions).float()  # [mention_num, slen] select_metrix[i][j] =1代表的就是第i个mention在位置j出现过，否则没有出现。select_metrix.size(1) 就是doc 的长度 
-            # average word -> mention  找出每行有几个数
+            # average word -> mention  找出每行有几个数。 相当于一个 average pool
             word_total_numbers = torch.sum(select_metrix, dim=-1).unsqueeze(-1).expand(-1, slen)  # [mention_num, slen]
             select_metrix = torch.where(word_total_numbers > 0, select_metrix / word_total_numbers, select_metrix)
             # 根据select_metrix 和 encoder_output 做乘法，得到实体的表示
@@ -353,51 +356,57 @@ class GAIN_BERT(nn.Module):
             else:
                 features = torch.cat((features, x), dim=0)
         # graph_big ：<class 'dgl.heterograph.DGLHeteroGraph'> 将一批graph(graphs)一起处理
-        graph_big = dgl.batch(graphs) # 将batch_hetero => batch
+        graph_big = dgl.batch(graphs) # 将源码中的batch_hetero => batch
         # graph_big = graph_big.to(torch.device('cuda:0')) # 将这个graph_big 
         output_features = [features]
-        # GPU
+        
+        # 如下这个是对图中节点的更新操作
         for GCN_layer in self.GCN_layers:
             features = GCN_layer(graph_big, {"node": features})["node"]  # [total_mention_nums, gcn_dim]
             output_features.append(features)
-        # 将三个feature 拼接到一起
+        
+        # 将三个feature 拼接到一起。三个feature分别是：Bert的最后一层 + 两层图卷积的结果。 它们的维度都是 [mention_num,808]
         output_feature = torch.cat(output_features, dim=-1)
 
         graphs = dgl.unbatch(graph_big) # unbatch_hetero => unbatch
-
-        # mention -> entity  entity2mention_table 这个是一篇doc中 entity的个数 和 mention 的形成的矩阵
+        # mention -> entity
+        # entity2mention_table 这个是一篇doc中 entity的个数 和 mention 的形成的矩阵。矩阵中的值是怎么样形成的？
+        # eneity2mention[i][j]=1 表示的就是第i个entity和 第 j 个 mention 是同一个实体，相反，如果没有联系，则为0
         entity2mention_table = params['entity2mention_table']  # list of [entity_num, mention_num]
         entity_num = torch.max(params['entity_id']) # 选择当前batch 中最大数目的entity 作为 entity_num （在dev_44.json 中，其值为31）
         entity_bank = get_cuda(torch.Tensor(bsz, entity_num, self.bank_size)) # 得到一个tensor
         global_info = get_cuda(torch.Tensor(bsz, self.bank_size)) # global_info 是干什么的？ => 获取整个文档的信息
-
+        # step 2. 融合形成 entity_graph
         cur_idx = 0
-        entity_graph_feature = None
+        entity_graph_feature = None  # 存储一个batch中所有entity_graph 中的entity的特征
         for i in range(len(graphs)):  # 对每个图都进行一次操作
-            # average mention -> entity  # 这个 entity2mention_label 本身就是float类型
-            select_metrix = entity2mention_table[i].float()  # [local_entity_num, mention_num]
-            select_metrix[0][0] = 1  # 为什么要赋值？
-            mention_nums = torch.sum(select_metrix, dim=-1).unsqueeze(-1).expand(-1, select_metrix.size(1))
-            select_metrix = torch.where(mention_nums > 0, select_metrix / mention_nums, select_metrix)
-            node_num = graphs[i].number_of_nodes('node')
-            entity_representation = torch.mm(select_metrix, output_feature[cur_idx:cur_idx + node_num]) # 去论文中读一下这个 entity_representation 是怎么获取的
+            # average mention -> entity  # 这个 entity2mention_tabel 本身就是float类型
+            select_metrix = entity2mention_table[i].float()  # [local_entity_num, mention_num]  对每个图（doc）进行操作， entity2mention_table[i] 表示的就是第i张图（doc）
+            select_metrix[0][0] = 1  # TODO 为什么要赋值？  => 好像是因为在创建 entity2mention 的时候，就多加了一个1，而且是从1开始，所以0就没有数
+            mention_nums = torch.sum(select_metrix, dim=-1).unsqueeze(-1).expand(-1, select_metrix.size(1)) # 做这个扩展操作是为了后面的除法
+            select_metrix = torch.where(mention_nums > 0, select_metrix / mention_nums, select_metrix)  # 这个操作同上面，也是为了保持每行的值相同 。这里就是为了计算每个下标该有多大的权重去获取最后的entity表示。相当于对 mention 的一个加权
+            node_num = graphs[i].number_of_nodes('node')  # 这个 node_num 就是 mention_num == select_metrix.size(1)。 
+            entity_representation = torch.mm(select_metrix, output_feature[cur_idx:cur_idx + node_num]) # 通过合并mention的特征就能获取到 entity_representation
+            # 同理，这个select_metrix.size(0) 就是entity_num 。所以这里就相当于给一篇doc中的所有的 entity 赋特征值。 将所有的entity 属性表示放到entity_bank 中
             entity_bank[i, :select_metrix.size(0) - 1] = entity_representation[1:]
             global_info[i] = output_feature[cur_idx]
             cur_idx += node_num
 
-            if entity_graph_feature is None:
-                entity_graph_feature = entity_representation[1:, -self.gcn_dim:]
+            if entity_graph_feature is None: #  self.gcn_dim = 808， 代表的含义就是从倒数第808个开始，即取最后808个数。我们知道，最后808 维度就是图神经网络第二层的输出结果
+                entity_graph_feature = entity_representation[1:, -self.gcn_dim:] # TODO 为啥是从1 开始？ => 可能是第0号结点是根节点，所以要排除掉
             else:
                 entity_graph_feature = torch.cat((entity_graph_feature, entity_representation[1:, -self.gcn_dim:]),
                                                  dim=0)
 
+        # 对下面这个 h_t_pairs 不理解，不清楚是什么意思？ => 其实就是h_entity 和 t_entity 组成的pair 对。
+        #  其size = [batch_size,h_t_limit,2] h_t_limit 代表的就 h_entity 和 t_entity 能组成的最大对的值。        
         h_t_pairs = params['h_t_pairs']
         h_t_pairs = h_t_pairs + (h_t_pairs == 0).long() - 1  # [batch_size, h_t_limit, 2]
-        h_t_limit = h_t_pairs.size(1)  # TODO 这个参数是什么意思？
+        h_t_limit = h_t_pairs.size(1)  # 这个参数是什么意思？ => 见上分析
 
-        # [batch_size, h_t_limit, bank_size]
         # TODO 找出 h_entity/t_entity 的index 过程 => 没看懂
-        h_entity_index = h_t_pairs[:, :, 0].unsqueeze(-1).expand(-1, -1, self.bank_size)
+        # [batch_size, h_t_limit, bank_size] 为什么要搞成这个形状
+        h_entity_index = h_t_pairs[:, :, 0].unsqueeze(-1).expand(-1, -1, self.bank_size) 
         t_entity_index = h_t_pairs[:, :, 1].unsqueeze(-1).expand(-1, -1, self.bank_size)
         # entity_bank = ([batch_size, 31, 2424])
         # [batch_size, h_t_limit, bank_size]
@@ -407,30 +416,31 @@ class GAIN_BERT(nn.Module):
 
         global_info = global_info.unsqueeze(1).expand(-1, h_t_limit, -1)
 
-        # 为啥还要从 params 中抽取出图？？ => 因为这里得到的是entity_graph， 还需要在它的基础上进行一个路径推理得到最后的结果
-        entity_graphs = params['entity_graphs'] 
-        entity_graph_big = dgl.batch(entity_graphs)
+        # 为啥还要从 params 中抽取出图？？ => 因为这里得到的是entity_graph， 还需要在它的基础上进行一个路径推理得到最后的结果。 现在对这个entity_graphs 还不是理解，这里的图只是整个batch 的图，是个list
+        entity_graphs = params['entity_graphs']  # 这里的 entity_graph 为啥只有一个？而不是 batch_size 个？
+        entity_graph_big = dgl.batch(entity_graphs) # batch之后，大图的节点个数就是entity_graphs 中各个小图的节点数和
         entity_graph_big = entity_graph_big.to(torch.device('cuda:0'))
-        self.edge_layer(entity_graph_big, entity_graph_feature)
+        self.edge_layer(entity_graph_big, entity_graph_feature) # entity_graph_feature.size(0) == entity_graph_big.num_nodes() 
 
         entity_graphs = dgl.unbatch(entity_graph_big)
         path_info = get_cuda(torch.zeros((bsz, h_t_limit, self.gcn_dim * 4)))
         relation_mask = params['relation_mask']
-        path_table = params['path_table']
+        path_table = params['path_table'] # 理解一下这个 path_table 的生成过程
 
-        # 下面这个操作很慢，是整个过程较为耗时的部分
-        for i in range(len(entity_graphs)):
-            path_t = path_table[i]
-            for j in range(h_t_limit):
+        # 下面这个操作很慢，是整个过程较为耗时的部分。 应该是为了想使用 path 进行推理        
+        for i in range(len(entity_graphs)): # i 表示第i张图
+            path_t = path_table[i]  # 第i张图的path 
+            for j in range(h_t_limit): # 感觉这个的含义就是 head_entity 和 tail_entity 能组成对的最大限制个数 就叫 h_t_limit
                 if relation_mask is not None and relation_mask[i, j].item() == 0:
                     break
-
+                # 根据上面的这个描述，就能理解： h_t_pairs[i,j,0] 就符合逻辑了
                 h = h_t_pairs[i, j, 0].item() # [batch_size,930,2]
                 t = h_t_pairs[i, j, 1].item()
                 # for evaluate
                 if relation_mask is None and h == 0 and t == 0:
                     continue
-
+                # 为什么这里要 h+1/t+1 ？ 然后后面又是 val-1？ => 因为在data.py 的create_entity_graph 中创建的path是按照(i+1,j+1) = (val+1) 创建的。所以这里要还原回去，就需要执行 h+1/t+1 。然后真正的节点下标是val-1，而不是val
+                # 虽然写成h+1,t+1 但是依然代表的是节点 h->t 
                 if (h + 1, t + 1) in path_t:
                     v = [val - 1 for val in path_t[(h + 1, t + 1)]]
                 elif (t + 1, h + 1) in path_t:
@@ -446,22 +456,24 @@ class GAIN_BERT(nn.Module):
 
                 middle_node_num = len(v)
 
-                if middle_node_num == 0:
+                if middle_node_num == 0: # 判断是否有中间节点
                     continue                            
                 # 这里分成 forward 和 backward 为什么？
-                # forward
-                edge_ids = get_cuda(entity_graphs[i].edge_ids([h for _ in range(middle_node_num)], v))
-                forward_first = torch.index_select(entity_graphs[i].edata['h'], dim=0, index=edge_ids)
+                # forward  这里的forward 分成两个方向，一个是从[h] -> [v]，得到的结果是 forward_first；一个是 从 v-> [t]，得到的结果是 forward_second。这就是文中说的两跳关系。 最后得到 h->t 的路径总和
+                # 使用for循环遍历middle_node_num 生成的是同个h组成的list
+                edge_ids = get_cuda(entity_graphs[i].edge_ids([h for _ in range(middle_node_num)], v)) # 获取这条路径上的所有边信息。 edge_ids([],[]) 传入的是两个list，得到边的ids。如果没有边，则报错
+                forward_first = torch.index_select(entity_graphs[i].edata['h'], dim=0, index=edge_ids)  # entity_graphs[i].edata['h'] 获取第i张图的边特征（h）
                 edge_ids = get_cuda(entity_graphs[i].edge_ids(v, [t for _ in range(middle_node_num)]))
                 forward_second = torch.index_select(entity_graphs[i].edata['h'], dim=0, index=edge_ids)
 
-                # backward
+                # backward => 为什么还要再回去一遍？这个就没啥道理了，感觉就像是单纯为了多拼接一点儿向量
                 edge_ids = get_cuda(entity_graphs[i].edge_ids([t for _ in range(middle_node_num)], v))
                 backward_first = torch.index_select(entity_graphs[i].edata['h'], dim=0, index=edge_ids)
                 edge_ids = get_cuda(entity_graphs[i].edge_ids(v, [h for _ in range(middle_node_num)]))
                 backward_second = torch.index_select(entity_graphs[i].edata['h'], dim=0, index=edge_ids)
-
+                # 竟然就是直接的拼接？
                 tmp_path_info = torch.cat((forward_first, forward_second, backward_first, backward_second), dim=-1)
+                # 下面这个就是文章中3.3 Entity-level Graph Inference Module 部分的(8)-(10) 三个公式对应的部分。使用attention 计算出一条综合的表示
                 _, attn_value = self.attention(torch.cat((h_entity[i, j], t_entity[i, j]), dim=-1), tmp_path_info)
                 path_info[i, j] = attn_value
 
@@ -473,6 +485,7 @@ class GAIN_BERT(nn.Module):
             )
         )
         # 下面这个部分对应的就是 3.4 Classification Module 中的内容，将拼接得到tensor 做一个predict操作
+        # torch.cat 之后得到的维度是 [batch_size,h_t_limit,15352]
         predictions = self.predict(torch.cat(
             (h_entity, t_entity, torch.abs(h_entity - t_entity), torch.mul(h_entity, t_entity), global_info, path_info),
             dim=-1))
@@ -545,8 +558,8 @@ class BiLSTM(nn.Module):
 
         return outputs, (output_h_t, output_c_t)
 
-'''这个类是什么类？
-(1)感觉像是 做GCN 的一层
+
+''' TODO 这个类是什么类？
 '''
 class RelGraphConvLayer(nn.Module):
     r"""Relational graph convolution layer.
@@ -559,7 +572,7 @@ class RelGraphConvLayer(nn.Module):
     rel_names : list[str]
         Relation names.
 
-    # TODO 这里的bases 是啥意思？
+    # TODO 这里的 bases 是啥意思？
     # 
     num_bases : int, optional
         Number of bases. If is none, use number of relations. Default: None.
@@ -597,6 +610,7 @@ class RelGraphConvLayer(nn.Module):
         # dglnn.HeteroGraphConv  是一个在异质图上计算卷积的通用模块
         # 这个定义的方式有点儿像是对 relation 进行操作。
         self.conv = dglnn.HeteroGraphConv({ 
+            # 这里对所有的 rel 都用的是同种卷积模块，即 GraphConv
             rel: dglnn.GraphConv(in_feat, out_feat, norm='right', weight=False, bias=False)
             for rel in rel_names
         })
@@ -638,14 +652,15 @@ class RelGraphConvLayer(nn.Module):
             New node features for each node type.
         """
         g = g.local_var() ## TODO ？
-        if self.use_weight:
-            weight = self.basis() if self.use_basis else self.weight # TODO？
+        if self.use_weight: # rel_name=['intra','inter','global']
+            weight = self.basis() if self.use_basis else self.weight # TODO？ size = [3,808,808]
             wdict = {self.rel_names[i]: {'weight': w.squeeze(0)}
                      for i, w in enumerate(torch.split(weight, 1, dim=0))}
         else:
             wdict = {}
-        hs = self.conv(g, inputs, mod_kwargs=wdict) # 
+        hs = self.conv(g, inputs, mod_kwargs=wdict) # 在wdict 这个字典中都执行一个预定义的Conv 操作。但是这个 wdict 是定义在边上的值，不是用节点的值吗？
 
+        # 在计算好的节点表示中，再进一步处理，比如添加自环，添加偏置再做激活和dropout
         def _apply(ntype, h):
             if self.self_loop:
                 h = h + torch.matmul(inputs[ntype], self.loop_weight)
@@ -658,8 +673,7 @@ class RelGraphConvLayer(nn.Module):
         return {ntype: _apply(ntype, h) for ntype, h in hs.items()}
 
 
-'''
-这个类同上面这个类的区别是什么？
+'''TODO 这个类同上面这个类的区别是什么？
 '''
 class RelEdgeLayer(nn.Module):
     def __init__(self,
@@ -676,7 +690,12 @@ class RelEdgeLayer(nn.Module):
 
     def forward(self, g, inputs):
         # g = g.local_var()
-        g.ndata['h'] = inputs  # [total_mention_num, node_feat]
+        g.ndata['h'] = inputs  # [total_mention_num, node_feat] 简单的赋值操作，为图中的每个节点都赋予feature
+        '''
+        apply_edges : update the features of the specified edges by the provied function
+            func:使用这个函数去产生新的边特征。可以是DGL内置的方法，也可以是自定义的方法
+            edges: 待更新表示的边。默认是更新图中的所有边
+        '''
         g.apply_edges(lambda edges: {
             'h': self.dropout(self.activation(self.mapping(torch.cat((edges.src['h'], edges.dst['h']), dim=-1))))})
         g.ndata.pop('h')
